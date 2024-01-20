e79bda8029aacb81f7bf2ff050d718822ccb354e
chessman <cjb15770595346@163.com>
2024-01-20 15:30:19 +0800

    feat 完成注册接口

---
+++ src/app.module.ts
@@ -0,0 +1,48 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { EmailModule } from "./email/email.module";
+import { RedisModule } from "./redis/redis.module";
+import { Permission } from "./user/entities/permission.entitie";
+import { Role } from "./user/entities/role.entitie";
+import { User } from "./user/entities/user.entitie";
+import { UserModule } from "./user/user.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: ["src/.env"],
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory(configService: ConfigService) {
+        return {
+          type: "mysql",
+          host: configService.get("mysql_server_host"),
+          port: configService.get("mysql_server_port"),
+          username: configService.get("mysql_server_username"),
+          password: configService.get("mysql_server_password"),
+          database: configService.get("mysql_server_database"),
+          synchronize: true,
+          logging: true,
+          entities: [User, Role, Permission],
+          poolSize: 10,
+          connectorPackage: "mysql2",
+          extra: {
+            authPlugin: "sha256_password",
+          },
+        };
+      },
+      inject: [ConfigService],
+    }),
+    UserModule,
+
+    RedisModule,
+    EmailModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
